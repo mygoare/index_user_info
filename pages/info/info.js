80f2d3b25df37e1320b7319c7a72b8fdc61f9e3b
mygoare <mygoare@gmail.com>
2018-12-28 12:02:41 +0800

    deal with the (index, user, info) logic

--- pages/info/info.js
+++ pages/info/info.js
@@ -1,5 +1,26 @@
+const app = getApp();
+
 Page({
+  data: {
+    answer1: '',
+    answer2: '',
+  },
+  onLoad: function() {
+    const { answer1 = '', answer2 = '' } = app.globalData.userInfo;
+
+    this.setData({
+      answer1,
+      answer2,
+    })
+  },
   formSubmit(e) {
-    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    console.log('form submit with data', e.detail.value)
+
+    app.globalData.userInfo = Object.assign({}, e.detail.value);
+    app.globalData.userInfoCollected = true;
+
+    console.log(app.globalData.userInfo, app.globalData.userInfoCollected);
+
+    wx.navigateBack({});
   }
 })
--- pages/user/user.js
+++ pages/user/user.js
@@ -1,1 +1,19 @@
-Page({})
+const { isEmpty } = require('../../libs/util');
+
+const app = getApp();
+
+Page({
+  data: {
+    title: '',
+    size: 20,
+    type: '',
+  },
+  onShow: function(){
+    const { userInfo, userInfoCollected } = app.globalData;
+
+    this.setData({
+      title: isEmpty(userInfo) ? 'No Info' : 'Has Info',
+      type: userInfoCollected ? 'success' : 'clear',
+    })
+  }
+})
